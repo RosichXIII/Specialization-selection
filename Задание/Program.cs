381353c9266fcbd0c3456302c7718db296f6444d
rosich <rosichxiii@mail.ru>
2022-08-09 23:34:19 +0300

    создан массив случайных строк

--- Задание/Program.cs
+++ Задание/Program.cs
@@ -2,5 +2,33 @@
 
 void Method()
 {
-    
+    string[] array = new string[15];
+
+    FillArray(array);
+    PrintArray(array);
+    //ThreeCharSearch(array);
+}
+
+void FillArray(string[] array)
+{
+    Random random = new Random();
+    int stringLength;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        stringLength = random.Next(1, 7);
+        for (int j = 0; j <= stringLength; j++)
+        {
+            array[i] = array[i] + ((char)random.Next('a', 'z' + 1)).ToString();
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("Массив");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "  ");
+    }
 }
